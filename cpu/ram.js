bf121443773fc19ce143baa596724cb74cd1a4e1
yoonsunyu <yoonsun.yu46@codestates.com>
2023-04-30 22:01:51 +0900

    basic section

--- cpu/ram.js
+++ cpu/ram.js
@@ -1,16 +1,16 @@
-const RAM_MAX_ADDR_SZ = 128;
-const RAM_WORD_SZ = 32; // 32bit , 64bit 컴퓨터 구분 
+export const RAM_MAX_ADDR_SZ = 256; // 0x00 ~ 0xFF
+export const RAM_WORD_SZ = 32; // 32bit , 64bit 컴퓨터 구분 
 
 // 16진수 데이터를 문자로 표현합니다.
 // 16진수 1개 = 4비트 = 1문자 이기 때문에 32bit의 경우는 8개로 표현됩니다.
-const RAM_WORD_DATA_SZ = RAM_WORD_SZ / 4;
+export const RAM_WORD_DATA_SZ = RAM_WORD_SZ / 4;
 
 let _RAM_INIT_DATA = ''
 for (let i = 0 ; i < RAM_WORD_DATA_SZ; i++) {
   _RAM_INIT_DATA += '0'
 }
 
-const RAM_INIT_DATA = _RAM_INIT_DATA; // 16진수 형태를 문자형으로 저장.
+export const RAM_INIT_DATA = _RAM_INIT_DATA; // 16진수 형태를 문자형으로 저장.
 
 let data = []
 
@@ -25,7 +25,7 @@ export function printRam() {
 }
 
 export function getRam(addr) {
-  return parseInt(data [addr], 16); 
+  return parseInt(data[addr], 16); 
 }
 
 export function setRamByStr(addr, value) {
