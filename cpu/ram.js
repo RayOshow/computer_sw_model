67c2db0a013bd0454c2dd0291fdcb683f192a123
yoonsunyu <yoonsun.yu46@codestates.com>
2023-05-06 02:06:38 +0900

    ram 변경 사항 적용

--- cpu/ram.js
+++ cpu/ram.js
@@ -5,12 +5,7 @@ export const RAM_WORD_SZ = 32; // 32bit , 64bit 컴퓨터 구분
 // 16진수 1개 = 4비트 = 1문자 이기 때문에 32bit의 경우는 8개로 표현됩니다.
 export const RAM_WORD_DATA_SZ = RAM_WORD_SZ / 4;
 
-let _RAM_INIT_DATA = ''
-for (let i = 0 ; i < RAM_WORD_DATA_SZ; i++) {
-  _RAM_INIT_DATA += '0'
-}
-
-export const RAM_INIT_DATA = _RAM_INIT_DATA; // 16진수 형태를 문자형으로 저장.
+export const RAM_INIT_DATA = 0x00; 
 
 let data = []
 
@@ -20,23 +15,14 @@ for(let i = 0; i < RAM_MAX_ADDR_SZ ; i++) {
 
 export function printRam() {
   for(let i = 0; i < RAM_MAX_ADDR_SZ ; i++) {
-   console.log('['+i+'] '+ data[i])
+   console.log('['+i+'] 0x'+ data[i].toString())
   }
 }
 
 export function getRam(addr) {
-  return parseInt(data[addr], 16); 
-}
-
-export function setRamByStr(addr, value) {
-
-  if( value.length > RAM_WORD_DATA_SZ) {
-    value = value.slice(0, RAM_WORD_DATA_SZ);
-  }
-  
-  data [addr] = value
+  return data[addr]
 }
 
 export function setRam(addr, value) {
-  data [addr] = value.toString(16)
+  data[addr] = value
 }
